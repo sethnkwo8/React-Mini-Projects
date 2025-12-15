b2a9fa40b5df1b2c89fc08dbb873edd21c5ce244
Seth Nkwo <sethnkwocool@gmail.com>
2025-12-15 10:45:11 +0100

    Posts list setup

---
+++ posts-list/src/components/PostList.jsx
@@ -0,0 +1,5 @@
+export default function PostList() {
+    <div className="min-h-screen flex flex-col items-center">
+
+    </div>
+}
---
+++ posts-list/src/hooks/useFetchPosts.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default function useFetchPosts(url) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null)
+
+    async function fetchPosts() {
+        try {
+            setLoading(true);
+            setError(null);
+
+            const response = await fetch(url);
+            const data = await response.json()
+            setData(data);
+        }
+        catch (error) {
+            setError(error)
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        fetchPosts();
+    }, [url]);
+
+    return { data, loading, error, refresh: fetchPosts }
+}
